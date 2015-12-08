8e24ba3d059589956878df4b8afbc79b23a15280
Dm Bochkanov <bochkanov.dm@gmail.com>
2015-12-09 00:26:29 +0300

    updater

--- common/src/main/java/com/hvost/startpage/support/CarouselRepository.java
+++ common/src/main/java/com/hvost/startpage/support/CarouselRepository.java
@@ -5,6 +5,7 @@ import com.hvost.startpage.Carousel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.NamedNativeQuery;
 import java.util.List;
 
 /**
@@ -14,4 +15,7 @@ public interface CarouselRepository extends JpaRepository<Carousel, Long> {
 
   @Query("select sc from Carousel sc where sc.active = true")
   List<Carousel> findByActive();
+
+  @Query(value = "select * from start_carousel c where c.isactive = 1 and c.created_at < DATE_SUB(NOW(), INTERVAL 7 Day)", nativeQuery = true)
+  List<Carousel> findLastWeek();
 }

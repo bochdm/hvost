a6cec092790e5cf54b38a396f4676b70fb10ed99
Dm Bochkanov <bochkanov.dm@gmail.com>
2015-12-10 00:27:36 +0300

    carousel

---
+++ site/src/main/java/com/hvost/automate/Updater.java
@@ -0,0 +1,55 @@
+package com.hvost.automate;
+
+import com.hvost.admin.AdminService;
+import com.hvost.blog.Post;
+import com.hvost.blog.support.PostService;
+import com.hvost.startpage.Carousel;
+import com.hvost.startpage.support.CarouselService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by kseniaselezneva on 08/12/15.
+ */
+@Component
+public class Updater {
+
+  @Autowired
+  CarouselService carouselService;
+
+  @Autowired
+  PostService postService;
+
+  @Autowired
+  AdminService adminService;
+
+//  @Scheduled(cron = "0 0 0 * * *")
+  @Scheduled(cron = "0 * * * * *")
+  public void carouselToBlog(){
+
+    List<Carousel> onlyActual = carouselService.getOnlyActual();
+
+    System.out.println("list.carousel.size = " + onlyActual.size());
+    for (Carousel carousel : onlyActual) {
+      System.out.println("carousel->" + carousel);
+      Post post = new Post();
+      post.setTitle(carousel.getTitle().replaceAll("\\<.*?>", ""));
+      post.setSummary(carousel.getContent());
+      post.setAuthor("admin");
+      String link = carousel.getLink();
+      if (link.contains("http")) {
+        post.setContent(carousel.getContent() + "<br/><a href='" + link + "'/>");
+      } else {
+        post.setContent(carousel.getContent());
+      }
+      adminService.addArticle(post);
+      System.out.println("move to blog ->" + post);
+      carousel.setActive(false);
+      adminService.updateCarousel(carousel);
+    }
+
+  }
+}
